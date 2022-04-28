f31f05d5998ab4b64881f0e69a1d2f22c76396e0
Tarsha Simmons <tarsha.simmons04@gmail.com>
2022-04-28 16:10:48 -0500

    completed occupation and DOB

--- app.js
+++ app.js
@@ -295,4 +295,25 @@ function searchByTraits(people){
         .join("\n"))
      traitChoose = prompt('keep choosing? yes or no: ')
      break;
+   
+     case "Occupation":
+        let traitOccupation = promptFor('Enter Occupation or skip: ', chars)
+        people = people.filter(function (el){
+            if(el.occupation == traitOccupation)
+                return true;
+        })
+        alert(people.map(function (person){
+            return `${person.firstName} ${person.lastName}`;
+            })
+            .join("\n"))
     
+            traitChoose = prompt('keep choosing? yes or no: ')
+            break;
+
+
+    case "DOB":
+     let traitDob = promptFor('Enter Date of Birth MM/DD/YEAR or skip: ', chars)
+        people = people.filter(function (el){
+          if (el.dob == traitDob)
+          return true;
+    })
